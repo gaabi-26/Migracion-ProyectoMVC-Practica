ab9a1b2c23596bad92763b6125e946b3bc1556e7
Gabriel Martinez <gaabi26092005@gmail.com>
2025-12-10 13:09:42 -0300

    Correccion paquetes Nugets. Crear primera vista Discos.

---
+++ discos-mvc/Controllers/DiscosController.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using negocio;
+using dominio;
+using System.Data.SqlClient;
+
+namespace discos_mvc.Controllers
+{
+    public class DiscosController : Controller
+    {
+        // GET: DiscosController
+        public ActionResult Index()
+        {
+            DiscoNegocio discoNegocio = new DiscoNegocio();
+            List<Disco> discos = discoNegocio.listar();
+            foreach (var item in discos)
+            {
+                item.FechaLanzamiento = item.FechaLanzamiento.Date;
+            }
+            return View(discos);
+        }
+
+        // GET: DiscosController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: DiscosController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: DiscosController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: DiscosController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: DiscosController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: DiscosController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: DiscosController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
--- dominio/Disco.cs
+++ dominio/Disco.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,10 +11,13 @@ namespace dominio
     {
         public int Id { get; set; }
         public string Titulo { get; set; }
+        [DisplayName("Fecha de Lanzamiento")]
         public DateTime FechaLanzamiento { get; set; }
         public int CantidadCanciones { get; set; }
+        [DisplayName("Imagen")]
         public string UrlTapa { get; set; }
         public Estilo Estilo { get; set; }
+        [DisplayName("Tipo de Edición")]
         public TipoEdicion TipoEdicion { get; set; }
     }
 }
